a5b884f52ce7d679688144660aad066c92be8bb7
Daffolap-824 <Daffolap-824@DESKTOP-0FLI380>
2021-11-25 14:42:55 +0530

    Add project files.

---
+++ Udemeyassignment/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Udemeyassignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Index(string arg="")
+        {
+            if(arg=="")
+            {
+                return View();
+            }
+
+            else if (arg=="sample")
+            {
+                return File("~/Sample.pdf","application/pdf");
+            }
+            else if (arg=="gotoabout")
+            {
+                return RedirectToAction("About");
+            }
+            else if (arg=="login")
+            {
+                return View("Login");
+            }
+            else
+            {
+                return Content("You have typed :"+arg);
+            }
+        }
+    }
+}
